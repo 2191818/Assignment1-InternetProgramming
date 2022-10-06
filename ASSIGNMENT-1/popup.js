f5acc9c7b58a1006e017f9fac7f0cb5d8efff3a5
Muhammad Arsalan Saeed <94261900+2191818@users.noreply.github.com>
2022-10-05 23:53:46 -0400

    Extra changes, created error page and popup using java

---
+++ ASSIGNMENT-1/popup.js
@@ -0,0 +1,10 @@
+function linkClick(){
+
+    alert("Error this button is not operational yet!");
+
+    if(document.getElementById("div").style.display == "none")
+        document.getElementById("div").style.display = "block";
+    else
+        document.getElementById("div").style.display = "none";
+
+}
